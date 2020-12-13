4e40ad3bc1241f0f3ffbb124545b87779cf04357
Alexandra Rogovenko <a.rogovenko@syberry.com>
2020-12-13 14:56:28 +0300

    code was changed

--- src/Output.java
+++ src/Output.java
@@ -2,7 +2,7 @@ public class Output {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Output # "+i);
+            System.out.println("My favourite number is "+ i*2);
         }
     }
 }
